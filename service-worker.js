51576cdac9498a29b153b82a7c1d32b6fd6d7d10
Remy Sharp <remy@remysharp.com>
2018-06-01 18:57:18 +0200

    bump

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // we'll version our cache (and learn how to delete caches in
 // some other post)
-const cacheName = 'v10::static';
+const cacheName = 'v11::static';
 
 self.addEventListener('install', e => {
   // once the SW is installed, go ahead and fetch the resources
